8ecfeb7d77f82648a852cd826f016f5a9278129e
vasko <vsterev@gmail.com>
2021-01-19 22:29:20 +0200

    fix auhorization in hotels routes

--- clickatell-node/routes/hotel.js
+++ clickatell-node/routes/hotel.js
@@ -1,10 +1,10 @@
 const { Router } = require('express');
 const hotelController = require('../controllers/hotel');
-// const auth = require('../utils/auth');
+const auth = require('../utils/auth');
 const router = Router();
 
 // router.get('/', contactController.get.all);
-router.get('/all', hotelController.get.allHotels);
+router.get('/all', auth(), hotelController.get.allHotels);
 // router.get('/details/:id', auth(), reservationController.get.details)
 // router.all('*', auth(), villaController.get.notFound);
 
